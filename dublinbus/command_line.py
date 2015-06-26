7cc81d3e825f35902d827357fb82e0d09dfc3704
Federico Ceratto <federico.ceratto@gmail.com>
2015-06-26 21:17:06 +0100

    Add CLI bus stop argument

--- dublinbus/command_line.py
+++ dublinbus/command_line.py
@@ -1,7 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+from argparse import ArgumentParser
 import dublinbus
 
 
+def parse_args():
+    ap = ArgumentParser()
+    ap.add_argument('stop_number', type=int)
+    return ap.parse_args()
+
+
 def main():
-    print(dublinbus.print_stop('00353'))
+    args = parse_args()
+    print(dublinbus.print_stop(args.stop_number))
+
+
+if __name__ == '__main__':
+    main()
--- dublinbus/stop.py
+++ dublinbus/stop.py
@@ -4,11 +4,15 @@ import urllib.parse
 from bs4 import BeautifulSoup
 from tabulate import tabulate
 
-url = 'http://rtpi.ie/Text/WebDisplay.aspx?stopRef='
+BASEURL = 'http://rtpi.ie/Text/WebDisplay.aspx?stopRef='
 
 
 def print_stop(stop_number):
-    current_url = url + stop_number
+    if isinstance(stop_number, int):
+        stop_number = str(stop_number)
+
+    stop_number = stop_number.zfill(5)
+    current_url = "%s%s" % (BASEURL, stop_number)
     data = urllib.request.urlopen(current_url).read()
     soup = BeautifulSoup(str(data))
     buses = []
